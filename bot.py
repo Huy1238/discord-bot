d5181764b0344192508a2e1498a6b3557c2c9e1b
Huy1238 <nguyendamgiahuy12@gmail.com>
2025-08-31 13:17:59 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+TOKEN = "MTQxMTI2NjAzMDk0ODQ1MDM2Nw.G3h3jH.UScROKlqPXBpeGR97KLx9Q52M-Ymx72gCvACYU"
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Bot đã đăng nhập dưới tên {bot.user}")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Chào {ctx.author.mention} 👋!")
+
+bot.run(TOKEN)
+commit message: Add bot.py
